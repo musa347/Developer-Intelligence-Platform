4afe07621abe2be0c1e41228e03f46395d9071bb
Ibrahim Musa <imusa@imusa-sg1-2.local>
2026-05-08 17:45:19 +0100

    update import

--- src/main/java/com/dip/service/OnnxEmbeddingBackend.java
+++ src/main/java/com/dip/service/OnnxEmbeddingBackend.java
@@ -12,8 +12,11 @@ import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.ArrayList;
